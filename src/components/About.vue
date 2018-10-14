fdcccf7bb3ae3d148b3f6663514a292664842f79
nagatech <cheesecakebot@yahoo.co.jp>
2018-10-15 01:59:58 +0900

    About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,4 +1,17 @@
 <template>
+  <div>
+    <h2>About</h2>
+    <div class="md-layout">
+      <div id="avater">
+        <img src="../assets/syobon.png">
+      </div>
+      <div>
+        <p>Name: Tatsuya Nagano</p>
+        <p>ScreenName: nagatech(ながてち)、nagatea(ながてぃー)</p>
+        <p>Grade: 東京工業大学 情報理工学院 数理・計算科学系 B3</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +21,9 @@ export default {
 </script>
 
 <style>
+#avater {
+  width: 200px;
+  height: 200px;
+  padding-right: 20px;
+}
 </style>
